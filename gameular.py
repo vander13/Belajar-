a49f483265cd6cd17e40bb5ef5c6f56a362b2664
vander13 <evanderpaskalis@gmail.com>
2024-07-21 23:23:24 +0800

    masih cek

--- gameular.py
+++ gameular.py
@@ -7,8 +7,20 @@ print(f'''selamat datang {nama_user } coba perhatikan dibawah ini
       (_) (_) (_) (_)''')
 pilih_ular = int (input(f'''silahkan masukan goa yang mana ada ularnya :'''))
 
-if pilih_ular == posisi_ular :
-    print(f'''SELAMAT {nama_user} anda telah memilih nomer {pilih_ular} goa yang ada ular {posisi_ular}''')
-else:
-    print(f'''ANDA GAGAL memilih ular {nama_user} posisi yang anda pilih {pilih_ular} anda akan ketemu ular
-          jika memilih andaposisi {posisi_ular}''')
+while True:
+    user_input = input("Do you want to continue? (yes/no): ")
+    if user_input.lower() in ["yes", "y"]:
+        print(f"Continuing nomor ular {posisi_ular}")
+        break
+    elif user_input.lower() in ["no", "n"]:
+        print("Exiting...")
+        break
+    else:
+        print("Invalid input. Please enter yes/no.")
+        
+        
+# if pilih_ular == posisi_ular :
+#     print(f'''SELAMAT {nama_user} anda telah memilih nomer {pilih_ular} goa yang ada ular {posisi_ular}''')
+# else:
+#     print(f'''ANDA GAGAL memilih ular {nama_user} posisi yang anda pilih {pilih_ular} anda akan ketemu ular
+#           jika memilih posisi {posisi_ular}''')
--- main.py
+++ main.py
@@ -1,24 +1,6 @@
-selamat_datang = 'SELAMAT DATANG DI GAME'
-
-print(f'** {selamat_datang} **')
-
-nomor_anda = 10
-
-if nomor_anda == 10:
-    print('anda benar')
-else:
-    print('anda salah')
-
-
-
-
-print ("======== GAMES ULAR =========")
-
-nama_user = input("masukan nama anda :")
-ular_posisi = 2
-
-print(f"Halo selamat datang di game ular {nama_user}")
-pilih_user = input (f'''silahkan pilih ular pada goa (_) (_) (_) (_) :''' 
-      )
-
-if pilih_user == 
+answer = input("Enter yes or no: ")
+if answer.lower() == "yes":
+    print("YES!!!")
+elif answer.lower() == "no":
+    print("Oh no")
+else: print("Please enter yes or no.")
